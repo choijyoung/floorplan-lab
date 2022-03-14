513c1800e7fd80c9e3fec235894f2754e5a5776c
Jaeyoung Choi <choi.jae.y@gmail.com>
2022-03-14 16:20:37 -0400

    finish lab

---
+++ src/FloorPlan.jsx
@@ -0,0 +1,21 @@
+import Bath from './Bath';
+import Bedroom from './Bedroom';
+import Kitchen from './Kitchen';
+import LivingRoom from './LivingRoom';
+
+
+const FloorPlan = () => {
+  return ( 
+    <>
+   <Bedroom bedNum={'1'} />
+  <Kitchen />
+  <Bath size={'Full'}/>
+  <Bedroom bedNum={'2'}/>
+  <LivingRoom />
+  <Bath size={'Half'}/>
+  <Bedroom bedNum={'3'}/>
+    </>
+   );
+}
+ 
+export default FloorPlan;
